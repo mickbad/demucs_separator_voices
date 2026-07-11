41e5750c64972f6c1814ce2c5df55184e15aa9a7
Mick <31075343+mickbad@users.noreply.github.com>
2026-07-11 11:00:03 +0200

    compilation linux via docker

---
+++ docker_compilation_linux.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# docker_compilation_linux.sh
+#
+# procédure de compilation avec la bonne version de python (3.12) pour Linux
+#
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+echo "=== Build Linux - Docker ==="
+
+# préparation script
+cat > /tmp/build_script.sh <<'EOF'
+#!/bin/bash
+
+apt update
+apt install -y python3 python3-venv git build-essential ffmpeg
+
+cd /app
+./build_linux.sh
+
+echo
+echo "=== Build terminé ==="
+
+# accès à la console 
+bash
+EOF
+
+docker run --rm -v "$(pwd)":/app -w /app python:3.12 bash /tmp/build_script.sh
+
+rm /tmp/build_script.sh
+
+echo
+echo "=== Build terminé ==="
+
+echo
+echo "Executable :"
+echo "  dist/demucs_separator"
+echo
+echo "Python utilisé :"
+echo "  3.12"
+echo
+echo "Architecture :"
+echo "  x86_64"
